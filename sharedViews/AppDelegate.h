b54c3872e5c0416c80b8faa676521e9c90c6747d
andy Hazlett <andyhaz1970@gmail.com>
2016-01-10 01:28:40 -0800

    Initial Commit

---
+++ sharedViews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sharedViews
+//
+//  Created by andrew hazlett on 1/10/16.
+//  Copyright © 2016 andrew hazlett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
